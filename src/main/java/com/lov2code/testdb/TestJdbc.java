fe8d8b839dab53696942155be8954f0836236dc1
kdh <kdh@gmail.com>
2018-03-31 16:51:25 +0900

    #4 Test Commit

--- src/main/java/com/lov2code/testdb/TestJdbc.java
+++ src/main/java/com/lov2code/testdb/TestJdbc.java
@@ -9,7 +9,6 @@ public class TestJdbc
 {
     public static void main(String[] args) 
     {
-
     	String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false";
         String user = "springstudent";
         String password = "springstudent";
@@ -18,8 +17,7 @@ public class TestJdbc
         {
             System.out.println("Connecting to database: "+ jdbcUrl);
 
-            Connection myConn =
-                    DriverManager.getConnection(jdbcUrl, user, password);
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, password);
 
             System.out.println("Connection successful !!!");
         }
